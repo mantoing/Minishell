9436784c1f117e6847fff9ceecba590ec9ac698e
Suhwan Kim <suhkim@c10r7s7.42seoul.kr>
2022-11-20 20:21:42 +0900

    test commit for gitignore

--- split_quote.c
+++ split_quote.c
@@ -6,44 +6,11 @@
 /*   By: suhkim <suhkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 01:46:44 by suhkim            #+#    #+#             */
-/*   Updated: 2022/11/18 05:04:31 by suhkim           ###   ########.fr       */
+/*   Updated: 2022/11/18 05:48:47 by suhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
-
-/*
-static int	quote_check(int	quote, int *i)
-{
-	if (quote == 1)
-	{
-		*i += 1;
-		return (0);
-	}
-	else if (quote == 2)
-		return (2);
-	else
-	{
-		*i += 1;
-		return (1);
-	}
-}
-
-static int	double_quote_check(int quote, int *i)
-{
-	if (quote == 1)
-		return (1);
-	else if (quote == 2)
-	{
-		*i += 1;
-		return (0);
-	}
-	else
-	{
-		*i += 1;
-		return (2);
-	}
-}*/
 
 char	*split_quote(t_info *info, char *target)
 {
@@ -54,26 +21,7 @@ char	*split_quote(t_info *info, char *target)
 	quote = 0;
 	i = 0;
 	str = ft_strdup("");
-	/*
-	while (*(target + i))
-	{
-		if (*(target + i) == '\'')
-			quote = quote_check(quote, &i);
-		else if (*(target + i) == '\"')
-			quote = double_quote_check(quote, &i);
-		if (quote == 1) // in single quote
-			str = ft_strjoin(str, ft_substr(target, i, 1));
-		else if (quote == 2) // in double quote
-		{
-			if (*(target + i) != '$')
-				str = ft_strjoin(str, ft_substr(target, i, 1));
-			else
-				str = ft_strjoin(str, get_env(info, target + i, &i));
-		}
-		else if (*(target + i))
-			str = ft_strjoin(str, ft_substr(target, i, 1));
-		i++;
-	}*/
+
 	while (*(target + i))
 	{
 		if (*(target + i) == '\'')
