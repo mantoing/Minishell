e03936c06277ffad061a55c0caa8ae6a3cfdc6c4
suhwan <tnghkswkd123@naver.com>
2022-11-20 20:21:42 +0900

    test commit for gitignore

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: suhkim <suhkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 00:26:59 by suhkim            #+#    #+#             */
-/*   Updated: 2022/11/18 04:08:35 by suhkim           ###   ########.fr       */
+/*   Updated: 2022/11/20 17:02:02 by suhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,40 @@
 
 void	parse(t_info *info, char *line)
 {
+	char	**arv;
+	char	*cmd;
+	char	*opt;
+	char	*arg;
+	int		pid;
+	int		status;
+
+	arv = malloc(sizeof(char *) * 4);
 	divide_space(&info->input, line);
-	printf("cmd = %s\n", info->input.cmd);
-	printf("opt = %s\n", info->input.opt);
-	printf("arg = %s\n", info->input.arg);
-	printf("after split cmd = %s\n",split_quote(info, info->input.cmd));
-	printf("after split opt = %s\n",split_quote(info, info->input.opt));
-	printf("after split arg = %s\n",split_quote(info, info->input.arg));
+//	printf("cmd = %s\n", info->input.cmd);
+//	printf("opt = %s\n", info->input.opt);
+//	printf("arg = %s\n", info->input.arg);
+	cmd = split_quote(info, info->input.cmd);
+	opt = split_quote(info, info->input.opt);
+	arg = split_quote(info, info->input.arg);
+//	printf("after split cmd = %s\n",cmd);
+//	printf("after split opt = %s\n",opt);
+//	printf("after split arg = %s\n",split_quote(info, info->input.arg));
+	pid = fork();
+	if (pid == 0)
+	{
+		if (!ft_strncmp(cmd, "echo", 4))
+		{
+		//	printf("\n i'm echo \n");
+			arv[0] = ft_strdup(opt);
+			arv[1] = ft_strdup(opt);
+			arv[2] = ft_strdup(arg);
+			arv[3] = NULL;
+			execve("/bin/echo", arv, NULL);
+		}
+		exit(1);
+	}
+	else
+	{
+		pid = wait(&status);
+	}
 }
