993f8f8c061bb12ea0a77eedc88616baf98927e8
jaeywon <dnsjwodyd@gmail.com>
2023-01-20 01:28:16 +0900

    norm utils

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jaeywon <jaeywon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:18:57 by jaeywon           #+#    #+#             */
-/*   Updated: 2023/01/20 01:24:03 by suhkim           ###   ########.fr       */
+/*   Updated: 2023/01/20 01:27:47 by jaeywon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,14 @@ int	sort_len(char **arr)
 
 int	is_dir(t_info *info, char *name)
 {
-	struct stat statbuf;
-    
-    stat(name, &statbuf);
+	struct stat	statbuf;
+
+	stat(name, &statbuf);
 	if (S_ISDIR(statbuf.st_mode))
 	{
 		info->exit_code = print_err_with_exit_num(name, \
 				"Is a directory", NULL, 126);
 		return (1);
 	}
-    return (2);
-
+	return (2);
 }
